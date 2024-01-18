817191692f1bcf60ee31464c667478e9758fe4df
Muhammad Haris <135629302+itsharisofficial291@users.noreply.github.com>
2024-01-18 14:14:52 +0500

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,3 +10,18 @@
         <a href="{{ route('calculate') }}">Percentage and Grade Calculator</a>
     </nav>
     </center>
+@php
+$name= ["Haris","Ahmed","Ansari","Haris","Ahmed","Ansari"]; 
+@endphp    
+
+@foreach ($name as $names)
+
+@if($loop->even)
+{{"S.no - $loop->iteration"}}<a style="color: rgb(197, 111, 12)" href="hars">{{$names}} <br></a>
+
+@elseif($loop->odd)
+{{"S.no - $loop->iteration"}}<a style="color: blue" href="hars">{{$names}} <br></a>
+@else
+<a style="color: rgb(255, 0, 0)" href="hars">{{$names}} <br></a>
+@endif
+@endforeach
